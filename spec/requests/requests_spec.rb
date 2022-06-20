0e04d28c09ec7d311064650f69ec18aaebad934a
Cerdtruir <ghoornabeel@gmail.com>
2022-06-20 20:27:50 +0200

    Test if the controller renders the correct text

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -12,6 +12,11 @@ describe 'requests to controllers' do
       expect(response).to render_template(:index)
     end
 
+    it 'should return the correct text on page when getting index' do
+      get '/users'
+      expect(response.body).to include('Users index')
+    end
+
     it 'should return a correct when getting a show page' do
       get '/users/1'
       expect(response).to have_http_status(:ok)
@@ -20,6 +25,11 @@ describe 'requests to controllers' do
     it 'should return the correct page when getting a show page' do
       get '/users/1'
       expect(response).to render_template(:show)
+    end
+
+    it 'should return the correct text on page when getting index' do
+      get '/users/1'
+      expect(response.body).to include('users show')
     end
   end
 
@@ -34,6 +44,11 @@ describe 'requests to controllers' do
       expect(response).to render_template(:index)
     end
 
+    it 'should return the correct text on page when getting index' do
+      get '/users/1/posts'
+      expect(response.body).to include('posts index')
+    end
+
     it 'should return a correct when getting a show page' do
       get '/users/1/posts/1'
       expect(response).to have_http_status(:ok)
@@ -42,6 +57,11 @@ describe 'requests to controllers' do
     it 'should return the correct page when getting a show page' do
       get '/users/1/posts/1'
       expect(response).to render_template(:show)
+    end
+
+    it 'should return the correct text on page when getting index' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('posts show')
     end
   end
 end
