26364996dc782049b8b2f928d55dca37fe4fb01d
Cerdtruir <ghoornabeel@gmail.com>
2022-06-20 20:21:47 +0200

    Test if controller renders correct template

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -3,13 +3,23 @@ require_relative '../rails_helper'
 describe 'requests to controllers' do
   context 'users requests' do
     it 'should return a correct response when getting index' do
-      get '/users/index'
+      get '/users'
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return the correct page when getting index' do
+      get '/users'
+      expect(response).to render_template(:index)
     end
 
     it 'should return a correct when getting a show page' do
       get '/users/1'
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return the correct page when getting a show page' do
+      get '/users/1'
+      expect(response).to render_template(:show)
     end
   end
 
@@ -19,9 +29,19 @@ describe 'requests to controllers' do
       expect(response).to have_http_status(:ok)
     end
 
+    it 'should return the correct page when getting index' do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
+    end
+
     it 'should return a correct when getting a show page' do
       get '/users/1/posts/1'
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return the correct page when getting a show page' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
     end
   end
 end
