38c8ae0c302aa80de8e4611be299af5c19ca7254
Cerdtruir <ghoornabeel@gmail.com>
2022-06-22 00:19:15 +0200

    Add update posts counter after save method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,15 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  after_save :update_post_counter
+
   def show_5_comments
     comments.order('created_at DESC').limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.count)
   end
 end
