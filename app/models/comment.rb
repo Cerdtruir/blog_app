092ed391045a0b8b76cf17f02108468ae3f5fe7f
Cerdtruir <ghoornabeel@gmail.com>
2022-06-22 00:18:28 +0200

    Add update comment counter after save method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comment_counter
+
+  private
+
+  def update_comment_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
