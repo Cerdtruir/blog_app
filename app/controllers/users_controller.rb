a757d0a0ea3ea4be1d4e6fa9569714091530bab5
Cerdtruir <ghoornabeel@gmail.com>
2022-06-24 00:30:40 +0200

    Update users controller to get data for views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @three_posts = User.find(params[:id]).show_3_posts
+    @all_posts = User.find(params[:id]).posts
+  end
 end
