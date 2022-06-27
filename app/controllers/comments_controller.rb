cce69a1f62eedff827537a2202cf34a4539b87ec
Cerdtruir <ghoornabeel@gmail.com>
2022-06-27 19:31:22 +0200

    Add new comment functionality to new controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @user = User.find(params[:user_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to user_post_path(@user, @post)
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(author: current_user, post: @post,
+                                                 text: "#{current_user.name}: #{params[:comment][:text]}")
+  end
+end
