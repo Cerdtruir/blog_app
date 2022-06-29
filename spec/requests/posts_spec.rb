5ac43cf1992ce772c2dcd2159bdd38474f7acb95
Cerdtruir <ghoornabeel@gmail.com>
2022-06-29 22:13:28 +0200

    fix request tests broken by new controller methods

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,36 +1,46 @@
 require_relative '../rails_helper'
+require_relative '../spec_helper'
 
 describe 'Posts', type: :request do
+  let!(:user) do
+    User.create(name: 'fadfasd', photo: 'a.jpeg', bio: 'Example bio', posts_counter: 0)
+  end
+
   context 'when getting index' do
     it 'should return a correct response' do
-      get '/users/1/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return the correct page' do
-      get '/users/1/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
 
     it 'should return the correct text on page' do
-      get '/users/1/posts'
-      expect(response.body).to include('posts index')
+      get "/users/#{user.id}/posts"
+      expect(response.body).to include('Number of posts:')
     end
 
     context 'when getting show page' do
+      let!(:post) do
+        Post.create(author: user, title: 'Example title', text: 'Example subject', comments_counter: 0,
+                    likes_counter: 0)
+      end
+
       it 'should return a correct response' do
-        get '/users/1/posts/1'
+        get "/users/#{user.id}/posts/#{post.id}"
         expect(response).to have_http_status(:ok)
       end
 
       it 'should return the correct page' do
-        get '/users/1/posts/1'
+        get "/users/#{user.id}/posts/#{post.id}"
         expect(response).to render_template(:show)
       end
 
       it 'should return the correct text on page' do
-        get '/users/1/posts/1'
-        expect(response.body).to include('posts show')
+        get "/users/#{user.id}/posts/#{post.id}"
+        expect(response.body).to include('Comments:')
       end
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../rails_helper'
+require_relative '../spec_helper'
 
 describe 'Users', type: :request do
   context 'when getting index' do
@@ -14,23 +15,27 @@ describe 'Users', type: :request do
 
     it 'should return the correct text on page' do
       get '/users'
-      expect(response.body).to include('Users index')
+      expect(response.body).to include("<ul class='user_list'>")
     end
 
     context 'when getting show page' do
+      let!(:user) do
+        User.create(name: 'fadfasd', photo: 'a.jpeg', bio: 'Example bio', posts_counter: 0)
+      end
+
       it 'should return a correct response' do
-        get '/users/1'
+        get "/users/#{user.id}"
         expect(response).to have_http_status(:ok)
       end
 
       it 'should return the correct page' do
-        get '/users/1'
+        get "/users/#{user.id}"
         expect(response).to render_template(:show)
       end
 
       it 'should return the correct text on page' do
-        get '/users/1'
-        expect(response.body).to include('users show')
+        get "/users/#{user.id}"
+        expect(response.body).to include('Number of posts:')
       end
     end
   end
