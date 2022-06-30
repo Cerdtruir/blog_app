a883789da02b1d83c354ac8e4dc9311a7bb944b1
Cerdtruir <ghoornabeel@gmail.com>
2022-06-30 12:29:53 +0200

    Check if signed in before liking post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,12 @@
 class LikesController < ApplicationController
   def create
-    @user = User.includes(:posts).find(params[:user_id])
-    @post = @user.posts.find(params[:post_id])
-    Like.create(author: @user, post: @post)
-    redirect_to user_post_path(@user, @post)
+    if user_signed_in?
+      @user = current_user
+      @post = @user.posts.find(params[:post_id])
+      Like.create(author: @user, post: @post)
+      redirect_to user_post_path(@user, @post)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
