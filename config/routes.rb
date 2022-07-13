46be8e3789c4824c79cdeb6baa3394441841d312
Cerdtruir <ghoornabeel@gmail.com>
2022-07-13 12:48:46 +0200

    Add route for comment index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create destroy show] do
-      resources :comments, only: %i[new create destroy]
+      resources :comments, only: %i[index new create destroy]
       resources :likes, only: %i[create]
     end
   end
